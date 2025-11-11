07f13bb900bf5b8ae8b7e238612a87ab0bd5686f
nicolasances <nicolas.matteazzi@gmail.com>
2025-11-11 08:53:02 +0100

    First version (empty)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { TotoAPIController } from "toto-api-controller";
 import { ControllerConfig } from "./Config";
 
-const api = new TotoAPIController(new ControllerConfig({ apiName: "toto-ms-ex1" }), { basePath: '/ex1' });
+const api = new TotoAPIController(new ControllerConfig({ apiName: "toto-ms-registry" }), { basePath: '/totoregistry' });
 
 // api.path('POST', '/something', new PostSomething())
 
